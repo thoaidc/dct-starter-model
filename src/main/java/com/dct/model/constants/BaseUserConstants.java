50d118659f82294a191204e9036a01c1d89241a5
thoaidc <dcthoai1023@gmail.com>
2025-09-02 10:47:59 +0700

    feat(constants): add function toString for user status

--- src/main/java/com/dct/model/constants/BaseUserConstants.java
+++ src/main/java/com/dct/model/constants/BaseUserConstants.java
@@ -1,11 +1,25 @@
 package com.dct.model.constants;
 
+import com.dct.model.exception.BaseIllegalArgumentException;
+
 @SuppressWarnings("unused")
 public interface BaseUserConstants {
+    String ENTITY_NAME = "com.dct.model.constants.BaseUserConstants";
+
     interface Status {
         byte INACTIVE = 0;
         byte ACTIVE = 1;
         byte LOCKED = 2;
         byte DELETED = 3;
+
+        static String toString(byte status) {
+            return switch (status) {
+                case INACTIVE -> "INACTIVE";
+                case ACTIVE -> "ACTIVE";
+                case LOCKED -> "LOCKED";
+                case DELETED -> "DELETED";
+                default -> throw new BaseIllegalArgumentException(ENTITY_NAME, "Unexpected user status: " + status);
+            };
+        }
     }
 }
