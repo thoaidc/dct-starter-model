2da0df07456a0fe316f63bf7ff91f720ec7dd58d
thoaidc <dcthoai1023@gmail.com>
2025-12-04 23:32:17 +0700

    fix

--- src/main/java/com/dct/model/constants/BaseKafkaConstants.java
+++ src/main/java/com/dct/model/constants/BaseKafkaConstants.java
@@ -3,20 +3,30 @@ package com.dct.model.constants;
 @SuppressWarnings("unused")
 public interface BaseKafkaConstants {
     interface Topic {
-        String CREATE_USER_SHOP_COMPLETION = "${spring.kafka.topics.create-user-shop-completion}";
-        String CREATE_USER_SHOP_FAILURE = "${spring.kafka.topics.create-user-shop-failure}";
-        String REGISTER_SHOP = "${spring.kafka.topics.register-shop}";
+        String USER_CREATED = "${spring.kafka.topics.user-created}";
+        String USER_REGISTER_SHOP_COMPLETION = "${spring.kafka.topics.user-register-shop-completion}";
+        String USER_REGISTER_SHOP_FAILURE = "${spring.kafka.topics.user-register-shop-failure}";
+        String ORDER_CREATED = "${spring.kafka.topics.order-created}";
+        String ORDER_PAYMENT_SUCCESSFUL = "${spring.kafka.topics.order-payment-successful}";
+        String ORDER_PAYMENT_FAILURE = "${spring.kafka.topics.order-payment-failure}";
     }
 
     interface GroupId {
-        String CREATE_USER_SHOP_COMPLETION = "create-user-shop-completion-group-id";
-        String CREATE_USER_SHOP_FAILURE = "create-user-shop-failure-group-id";
-        String REGISTER_SHOP = "register-shop-group-id";
+        String USER_CREATED = "user-created-group-id";
+        String USER_REGISTER_SHOP_COMPLETION = "user-register-shop-completion-group-id";
+        String USER_REGISTER_SHOP_FAILURE = "user-register-shop-failure-group-id";
+        String ORDER_CREATED = "${spring.kafka.topics.order-created}";
+        String ORDER_PAYMENT_SUCCESSFUL = "${spring.kafka.topics.order-payment-successful}";
+        String ORDER_PAYMENT_FAILURE = "${spring.kafka.topics.order-payment-failure}";
     }
 
+    // Number of consumers processing message concurrent
     interface Consumers {
-        String CREATE_USER_SHOP_COMPLETION = "1"; // Number of consumers processing message concurrent
-        String CREATE_USER_SHOP_FAILURE = "1"; // Number of consumers processing message concurrent
-        String REGISTER_SHOP = "1"; // Number of consumers processing message concurrent
+        String USER_CREATED = "1";
+        String USER_REGISTER_SHOP_COMPLETION = "1";
+        String USER_REGISTER_SHOP_FAILURE = "1";
+        String ORDER_CREATED = "1";
+        String ORDER_PAYMENT_SUCCESSFUL = "1";
+        String ORDER_PAYMENT_FAILURE = "1";
     }
 }
